bcf0c036af45fc1c6d1dc779cbb4e703a3c8f4ea
TweepCoding <coolguygaming170@gmail.com>
2020-04-20 01:16:29 -0500

    Fixed some stuff, added a panda...

--- src/main/java/dev/j3fftw/luckypanda/surprise/ObsidianTrapSurprise.java
+++ src/main/java/dev/j3fftw/luckypanda/surprise/ObsidianTrapSurprise.java
@@ -20,16 +20,18 @@ public class ObsidianTrapSurprise implements Surprise {
     @Override
     public void process(@Nonnull Player player, @Nonnull Block block) {
         Location playerLoc = player.getLocation();
+        Material blockMaterial;
         for (byte y = -1; y < 3; y++) {
             for (byte x = -1; x < 2; x++) {
                 for (byte z = -1; z < 2; z++) {
                     if (x == 0 && z == 0 && (y == 0 || y == 1)) {
-                        playerLoc.clone().add(x, y, z).getBlock().setType(Material.WATER);
+                        blockMaterial = Material.WATER;
                     } else if (((x == 0 && z != 0) || (z == 0 && x != 0)) && y == 1) {
-                        playerLoc.clone().add(x, y, z).getBlock().setType(Material.GLASS_PANE);
+                        blockMaterial = Material.GLASS_PANE;
                     } else {
-                        playerLoc.clone().add(x, y, z).getBlock().setType(Material.OBSIDIAN);
+                        blockMaterial = Material.OBSIDIAN;
                     }
+                    playerLoc.clone().add(x,y,z).getBlock().setType(blockMaterial);
                 }
             }
         }
--- src/main/java/dev/j3fftw/luckypanda/surprise/RollingPandaSurprise.java
+++ src/main/java/dev/j3fftw/luckypanda/surprise/RollingPandaSurprise.java
@@ -7,13 +7,20 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Panda;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
 
 import javax.annotation.Nonnull;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class RollingPandaSurprise implements Surprise {
 
-    private final Class<?> nmsClass = ReflectionUtils.getNMSClass("Panda");
+    private final Class<?> nmsClass = ReflectionUtils.getNMSClass("EntityPanda");
     private final Class<?> cbPandaClass = ReflectionUtils.getCBClass("entity.CraftPanda");
+    private static boolean taskRunning = false;
+    private static HashSet<Panda> pandaHashSet = new HashSet<>();
+
 
     @Nonnull
     @Override
@@ -23,10 +30,39 @@ public class RollingPandaSurprise implements Surprise {
 
     @Override
     public void process(@Nonnull Player player, @Nonnull Block block) {
-        Panda newPanda = (Panda) player.getWorld().spawnEntity(block.getLocation(), EntityType.PANDA);
-        ReflectionUtils.invokeMethod(nmsClass,
-            ReflectionUtils.invokeMethod(cbPandaClass, newPanda, "getHandle"),
-            "roll", new Class[] {Boolean.class}, true);
+        final Panda newPanda = (Panda) player.getWorld().spawnEntity(block.getLocation(), EntityType.PANDA);
+        newPanda.setMainGene(Panda.Gene.PLAYFUL);
+        pandaHashSet.add(newPanda);
+        if (!taskRunning) {
+            taskRunning = true;
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    Iterator<Panda> pandaIterator = pandaHashSet.iterator();
+                    while (pandaIterator.hasNext()) {
+                        Panda panda = pandaIterator.next();
+                        if (!panda.getLocation().getChunk().isLoaded())
+                            continue;
+                        else if (panda.isDead())
+                            pandaHashSet.remove(panda);
+                        else {
+                            final Object nmsPanda = ReflectionUtils.invokeMethod(cbPandaClass,
+                                panda, "getHandle");
+                            if (!(boolean) ReflectionUtils.invokeMethod(nmsClass, nmsPanda,
+                                "eC")) {
+                                ReflectionUtils.invokeMethod(nmsClass, nmsPanda, "v",
+                                    new Class[] {boolean.class}, true);
+                                panda.setVelocity(panda.getVelocity().add
+                                    (panda.getLocation().getDirection().normalize().multiply(0.01)));
+                            }
+                        }
+                    }
+                    if (pandaHashSet.isEmpty()) {
+                        taskRunning = false;
+                        cancel();
+                    }
+                }
+            }.runTaskTimer(LuckyPanda.getInstance(), 0, 1);
+        }
     }
-
 }
